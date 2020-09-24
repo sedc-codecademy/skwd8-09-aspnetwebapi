16cdfe8e7d4e0e5f4a4f1ca446fd4b56169d1e13
Risto Panchevski <Risto.Pancevski@stas.be>
2020-09-24 20:52:27 +0200

    Class 01

---
+++ g5/Class01/Class01/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return new List<User>()
+            {
+                new User()
+                {
+                    FirstName = "Risto",
+                    LastName = "Panchevski"
+                },
+                new User()
+                {
+                    FirstName = "Petko",
+                    LastName = "Petkovski"
+                }
+            };
+        }
+
+        [HttpPost]
+        public User Add(User user)
+        {
+            return user;
+        }
+    }
+}
---
+++ g5/Class01/Class01/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Class01
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
