c59d16d3e9cca858a8f648ca140c78d87f3ecb49
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-09-26 12:34:36 +0200

    added html js files

--- g6/Class02/SEDC.WebApi.Class02/SEDC.WebApi.Class02.Application/Controllers/NoteObjectsController.cs
+++ g6/Class02/SEDC.WebApi.Class02/SEDC.WebApi.Class02.Application/Controllers/NoteObjectsController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+using Newtonsoft.Json;
 using SEDC.WebApi.Class02.Application.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -32,5 +35,47 @@ namespace SEDC.WebApi.Class02.Application.Controllers
                 }
             }
         };
+
+        [HttpGet]
+        public ActionResult<List<Note>> Get()
+        {
+            return Ok(notes);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Note> Get(int id)
+        {
+            // should have error handling
+            return notes[id - 1];
+        }
+
+        // .../api/NotesObjcects/5(note id)/tags
+        [HttpGet("{noteId}/tags")]
+        public ActionResult<List<Tag>> Tags(int noteId)
+        {
+            // should have some error handling
+            return notes[noteId - 1].Tags;
+        }
+
+        // .../api/NotesObjcects/5(note id)/tags/2(tag id)
+        [HttpGet("{noteId}/tags/{tagId}")]
+        public ActionResult<Tag> Tag(int noteId, int tagId)
+        {
+            // should have error hendling
+            return notes[noteId - 1].Tags[tagId - 1];
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            string bodyContent = string.Empty;
+            using (var sr = new StreamReader(Request.Body))
+            {
+                bodyContent = sr.ReadToEnd();
+            }
+            Note note = JsonConvert.DeserializeObject<Note>(bodyContent);
+            notes.Add(note);
+            return Ok($"NOte with id: {notes.Count - 1} was added");
+        }
     }
 }
