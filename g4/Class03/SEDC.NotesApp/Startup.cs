3d6bc5bed480733700549aa10e42308fb4a2fa90
tstojanovska <stojanovskatanja03@gmail.com>
2020-09-28 21:33:18 +0200

    Class 03

---
+++ g4/Class03/SEDC.NotesApp/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SEDC.NotesApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "To Do List API V1");
+                });
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+            app.UseMvc();
+        }
+    }
+}
---
+++ g4/Class03/SEDC.NotesApp/StaticDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SEDC.NotesApp.Models;
+
+namespace SEDC.NotesApp
+{
+    public class StaticDb
+    {
+        public static List<Note> Notes = new List<Note>()
+        {
+            new Note(){ Text = "Do Homework", Color = "blue", Tags = new List<Tag>()
+                {
+                    new Tag(){ Name = "HomeWork", Color = "cyan"},
+                    new Tag(){ Name = "SEDC", Color = "blue"}
+                }
+            },
+            new Note(){ Text = "Drink more Water", Color = "blue", Tags = new List<Tag>()
+                {
+                    new Tag(){ Name = "Healthy", Color = "orange"},
+                    new Tag(){ Name = "Priority High", Color = "red"}
+                }
+            },
+            new Note(){ Text = "Go to the gym", Color = "blue", Tags = new List<Tag>()
+                {
+                    new Tag(){ Name = "exercise", Color = "blue"},
+                    new Tag(){ Name = "Priority Medium", Color = "yellow"}
+                }
+            }
+        };
+    }
+}
