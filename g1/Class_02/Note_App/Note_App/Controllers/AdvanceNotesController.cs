cb59cf5e33fb4258003f36bd30b2d95a352f32b1
Darko Panchevski <darko.panchevski@gmail.com>
2020-09-29 22:30:27 +0200

    G1 - Class 02

---
+++ g1/Class_02/Note_App/Note_App/Controllers/AdvanceNotesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Note_App.Models;
+
+namespace Note_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdvanceNotesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Note>> GetNotes()
+        {
+            return Ok(StaticDb.AdvanceNotes);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Note> GetById(int id)
+        {
+            if (id < 1) return StatusCode(StatusCodes.Status400BadRequest,
+               new { message = "Id cant be lower then one" });
+            var note = StaticDb.AdvanceNotes.SingleOrDefault(n => n.Id == id);
+            if (note == null)
+                return StatusCode(StatusCodes.Status404NotFound);
+
+            return Ok(note);
+
+        }
+        [HttpPost]
+        public ActionResult CreateNote()
+        {
+            try
+            {
+                using(StreamReader sr = new StreamReader(Request.Body))
+                {
+                    var jsonBody = sr.ReadToEnd();
+                    var note = JsonConvert.DeserializeObject<Note>(jsonBody);
+                    StaticDb.AdvanceNotes.Add(note);
+                    return StatusCode(StatusCodes.Status201Created, "Note successfully created");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ g1/Class_02/Note_App/Note_App/StaticDb.cs
@@ -0,0 +1,60 @@
+﻿using Note_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Note_App
+{
+    public static class StaticDb
+    {
+        public static List<string> GetSimpleNotes = new List<string>()
+        {
+            "Go to school",
+            "Go to gym",
+            "Do the dishes"
+        };
+
+        public static List<Note> AdvanceNotes = new List<Note>()
+        {
+            new Note()
+            {
+                Id = 1,
+                Text = "Do Homework",
+                Color = "green",
+                Tags = new List<Tag>()
+                {
+                    new Tag()
+                    {
+                        Text = "Obligatory",
+                        Color = "red"
+                    },
+                    new Tag()
+                    {
+                        Text = "SEDC",
+                        Color = "green"
+                    }
+                }
+            },
+            new Note()
+            {
+                Id = 2,
+                Text = "Go to gym",
+                Color = "green",
+                Tags = new List<Tag>()
+                {
+                    new Tag()
+                    {
+                        Text = "Obligatory",
+                        Color = "red"
+                    },
+                    new Tag()
+                    {
+                        Text = "SEDC",
+                        Color = "green"
+                    }
+                }
+            }
+        };
+    }
+}
