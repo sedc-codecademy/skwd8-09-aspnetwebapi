9e56ca765e08610707a6c90797deb838e0be2aee
Martin Panovski <panovski62@gmail.com>
2020-09-30 21:07:03 +0200

    Class 02 demo code part 2 added

--- g2/Class02/Class02.WebApi.Demo/Class02.WebApi/Controllers/NoteObjectsController.cs
+++ g2/Class02/Class02.WebApi.Demo/Class02.WebApi/Controllers/NoteObjectsController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Class02.WebApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Class02.WebApi.Controllers
 {
@@ -33,5 +35,77 @@ namespace Class02.WebApi.Controllers
                 }
             }
         };
+
+        [HttpGet]
+        public ActionResult<List<Note>> Get()
+        {
+            return notes;
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Note> Get(int id)
+        {
+            try
+            {
+                return notes[id - 1];
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return NotFound($"Note with Id {id} does not exist");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("BROKEN REQUEST - " + ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            string body;
+            using (StreamReader sr = new StreamReader(Request.Body))
+            {
+                body = sr.ReadToEnd();
+            }
+            Note note = JsonConvert.DeserializeObject<Note>(body);
+            notes.Add(note);
+            return Ok($"Note with id {notes.Count - 1} has been added!");
+        }
+
+        [HttpGet("{id}/tags")]
+        public ActionResult<List<Tag>> Tags(int id)
+        {
+            try
+            {
+                return notes[id - 1].Tags;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return NotFound($"There are no tags for note with Id {id}");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest("BROKEN REQUEST - " + ex.Message);
+            }
+        }
+
+        [HttpGet("{noteId}/tags/{tagId}")]
+        public ActionResult<Tag> Tags(int noteId, int tagId)
+        {
+            try
+            {
+                return notes[noteId - 1].Tags[tagId - 1];
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return NotFound($"There is no tag with Id {tagId} for note with Id {noteId}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("BROKEN REQUEST - " + ex.Message);
+            }
+        }
     }
 }
