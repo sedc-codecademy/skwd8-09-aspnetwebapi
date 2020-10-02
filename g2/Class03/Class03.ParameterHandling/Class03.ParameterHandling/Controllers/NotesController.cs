823436476aa69d37773192ef313dbe49c611ad4a
Martin Panovski <panovski62@gmail.com>
2020-10-02 19:04:44 +0200

    Class 03 demo code part 1 code added

---
+++ g2/Class03/Class03.ParameterHandling/Class03.ParameterHandling/Controllers/NotesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class03.ParameterHandling.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        public static List<Note> notes = new List<Note>()
+        {
+            new Note(){ Text = "Don't forget to water the plant", Color = "blue", Tags = new List<Tag>()
+                {
+                    new Tag(){ Name = "Home", Color = "cyan"},
+                    new Tag(){ Name = "Priority Low", Color = "green"}
+                }
+            },
+            new Note(){ Text = "Drink more Tea", Color = "blue", Tags = new List<Tag>()
+            {
+                    new Tag(){ Name = "Misc", Color = "orange"},
+                    new Tag(){ Name = "Priority Low", Color = "green"}
+                }
+            },
+            new Note(){ Text = "Make a break every 1h of coding", Color = "blue", Tags = new List<Tag>()
+            {
+                    new Tag(){ Name = "work", Color = "blue"},
+                    new Tag(){ Name = "Priority Medium", Color = "yellow"}
+                }
+            }
+        };
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Note>> Get()
+        {
+            return notes;
+        }
+
+
+        [HttpGet("{queryId}")]
+        public ActionResult<Note> GetIdQuery(int id)
+        {
+            try
+            {
+                return notes[id - 1];
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return NotFound($"Note with id: {id} not found!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"BROKEN REQUEST: {ex.Message}");
+            }
+        }
+
+        //[HttpGet("{id}")]
+        //public ActionResult<Note> Get(int id)
+        //{
+        //    try
+        //    {
+        //        return notes[id - 1];
+        //    }
+        //    catch (ArgumentOutOfRangeException)
+        //    {
+        //        return NotFound($"Note with id: {id} not found!");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest($"BROKEN REQUEST: {ex.Message}");
+        //    }
+        //}
+    }
+}
