d2ba37092a67b913174309b799e33e997a66e74c
Martin Panovski <panovski62@gmail.com>
2020-10-03 13:37:45 +0200

    Class 3 routes extra examples added

---
+++ g2/Class03/RoutesExtraExamples/Class03.EntityFramework/Class03.EntityFramework/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class03.EntityFramework.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public Book(int id, string title)
+        {
+            this.Id = id;
+            this.Title = title;
+        }
+    }
+}
