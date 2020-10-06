e3f557004262a4d387c5c09e0e7cb44b52299e4c
Filip <fjanev14@gmail.com>
2020-10-06 23:35:40 +0200

    added code for class4 exercise 1

---
+++ g3/class04/SEDC.Class04-ex1/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+
+        public virtual Customer Customer{get;set;}
+    }
+}
