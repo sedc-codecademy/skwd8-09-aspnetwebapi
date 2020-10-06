53b1760acfb9dfdd269d740f53e0612b998e67d2
ivletest <karakan@protonmail.com>
2020-10-06 08:22:03 +0200

    Updataded Class 5

---
+++ g7/Class 5/StickyNotes/StickyNotes.Database/Tables/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [CreatedOn] SMALLDATETIME NOT NULL, 
+    [DeletedOn] SMALLDATETIME NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Username] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL,
+)
