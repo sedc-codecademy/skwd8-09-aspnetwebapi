75f35d17d228eb762a15042f3e92a7871193a036
Risto Panchevski <Risto.Pancevski@stas.be>
2020-10-06 13:06:04 +0200

    Db Connection change

--- g5/Class04/PremierLeague/PremierLeague.DataAccess/PremierLeague.DataAccess/DbAccess/PremierLeagueContext.cs
+++ g5/Class04/PremierLeague/PremierLeague.DataAccess/PremierLeague.DataAccess/DbAccess/PremierLeagueContext.cs
@@ -23,7 +23,7 @@ namespace PremierLeague.DataAccess.PremierLeague.DataAccess.DbAccess
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=localhost\\SQLExpress;Database=PremierLeague;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=.;Database=PremierLeague;Trusted_Connection=True;");
             }
         }
 
