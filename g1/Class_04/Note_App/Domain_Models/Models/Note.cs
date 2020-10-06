05fa62695812e62027edb3603a60ed14e993c3c2
Darko Panchevski <darko.panchevski@gmail.com>
2020-10-06 13:24:32 +0200

    Initial commit and initial creation of class libraries for class 04 - G1

---
+++ g1/Class_04/Note_App/Domain_Models/Models/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Domain_Models.Enums;
+
+namespace Domain_Models.Models
+{
+    [Table("Notes")]
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string Text { get; set; }
+        public string Color { get; set; }
+        public TagType Tag { get; set; }
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+    }
+}
