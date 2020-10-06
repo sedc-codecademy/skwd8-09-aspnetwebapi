2a43017cd031343b7fcf8e634c14b5af2455b4ba
dblazeski96 <dblazeski96@gmail.com>
2020-10-06 21:54:43 +0200

    Added code for class 4

---
+++ g3/class04/SEDC.Class04/Models/Dto/NoteDtoGetAll.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Dto
+{
+    public class NoteDtoGetAll
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
---
+++ g3/class04/SEDC.Class04/Repository/NotesRepository.cs
@@ -0,0 +1,37 @@
+﻿using Models;
+using Models.Dto;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class NotesRepository : INotesRepository
+    {
+        public Note Add(NoteDtoAdd model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Note Edit(NoteDtoEdit model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Note Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<NoteDtoGetAll> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
