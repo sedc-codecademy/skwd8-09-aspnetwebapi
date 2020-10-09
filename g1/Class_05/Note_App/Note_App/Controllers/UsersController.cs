420a4a001958c51f1b614f398049d9565499d39d
Goce Kabov <Goce.Kabov@seavus.com>
2020-10-09 22:58:28 +0200

    updated class 05

---
+++ g1/Class_05/Note_App/Note_App/Controllers/UsersController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain_Models.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Note_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public ActionResult<List<User>> Get()
+        {
+            try
+            {
+                var users = _userService.GetAllUsers();
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            try
+            {
+                var user = _userService.GetUserById(id);
+                if (user == null) return BadRequest("Invalid user id");
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public ActionResult Post([FromBody] User user)
+        {
+            try
+            {
+                _userService.AddUser(user);
+                return Ok("User created successfully");
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut]
+        public ActionResult Put([FromBody] User user)
+        {
+            try
+            {
+                _userService.UpdateUser(user);
+                return Ok("User updated successfully");
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                _userService.DeleteUser(id);
+                return Ok("User deleted successfully");
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
