faa8e0bd5e7a7c843fdfae22f93d959be37e4d0f
dblazeski96 <dblazeski96@gmail.com>
2020-10-13 23:57:14 +0200

    Added code from class 6. Refactor and added comments

---
+++ g3/class06/NoteSolution/Data/NoteDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class NoteDbContext : DbContext
+    {
+        public NoteDbContext(DbContextOptions<NoteDbContext> options) : base(options) { }
+
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ g3/class06/NoteSolution/Repositories/Interfaces/INoteRepository.cs
@@ -0,0 +1,18 @@
+﻿using Models.Dto;
+using Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repositories.Interfaces
+{
+    public interface INoteRepository
+    {
+        IQueryable<Note> GetAll();
+        Note GetById(Guid noteId);
+        Note Add(Note note);
+        Note Edit(NoteEditDto noteDto);
+        int Delete(Guid noteId);
+    }
+}
