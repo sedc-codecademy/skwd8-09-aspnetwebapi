592cb35c00bf067c772adc6e65bdc4f03610c1a1
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-10-17 12:49:43 +0200

    added program.cs

--- g6/SEDC.WebApi.NoteApp/SEDC.WebApi.NoteApp.Api/Program.cs
+++ g6/SEDC.WebApi.NoteApp/SEDC.WebApi.NoteApp.Api/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting;
 
 namespace SEDC.WebApi.NoteApp.Api
 {
@@ -14,11 +17,22 @@ namespace SEDC.WebApi.NoteApp.Api
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Information()
+                .WriteTo.RollingFile(
+                    $@"{AppDomain.CurrentDomain.BaseDirectory}Logs\Notes_LOG_{DateTime.UtcNow.Date:dd-MM-yyyy}.txt",
+                    LogEventLevel.Information,
+                    "{NewLine}{Timestamp:HH:mm:ss} [{Level}] ({CorrelationToken} {Message}{NewLine}{Exception})"
+                ).CreateLogger();
+                
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .UseStartup<Startup>();
     }
 }
